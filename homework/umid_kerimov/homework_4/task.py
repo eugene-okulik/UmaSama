af9f506c30afb340001ca42a5689e6ec90b3ae8b
umasama <umidkerimov2@gmail.com>
2026-02-11 12:21:21 +0300

    Выполнение домашнего задания №4

--- homework/umid_kerimov/homework_4/task.py
+++ homework/umid_kerimov/homework_4/task.py
@@ -3,7 +3,7 @@ my_dict = {'tuple': (1, 2, 3, 4, 5), 'list': ['text', 7, False, 2.42, 5],
 print(my_dict['tuple'][-1])
 my_dict['list'].append(6)
 my_dict['list'].pop(1)
-my_dict['dict']['i am a tuple'] = 6
+my_dict['dict'][('i am a tuple',)] = 6
 my_dict['dict'].pop('two')
 my_dict['set'].add(6)
 my_dict['set'].pop()
