8318b94c03f383fc936610642dd6d02e1d0cdf61
umasama <umidkerimov2@gmail.com>
2026-02-16 21:23:16 +0300

    Выполение домашнего задания №7

---
+++ homework/umid_kerimov/homework_7/task3.py
@@ -0,0 +1,13 @@
+result1 = 'результат операции: 42'
+result2 = 'результат операции: 514'
+result3 = 'результат работы программы: 9'
+result4 = 'результат: 2'
+
+
+def result_sum(result):
+    colon_index = result.index(':')
+    number = int(result.index(':')[colon_index + 1:]) + 10
+    print(number)
+
+
+result_sum(result4)
