8e53d456fe46e513d1955f5cba32886401069830
umasama <umidkerimov2@gmail.com>
2026-02-17 10:05:28 +0300

    Выполение домашнего задания №7

--- homework/umid_kerimov/homework_7/task3.py
+++ homework/umid_kerimov/homework_7/task3.py
@@ -3,8 +3,13 @@ result2 = 'результат операции: 514'
 result3 = 'результат работы программы: 9'
 result4 = 'результат: 2'
 
+answer = [result1, result2, result3, result4]
 
-def result_sum(result):
-    colon_index = result.index(':')
-    number = int(result.index(':')[colon_index + 1:]) + 10
-    print(number)
+
+def result_sum(word):
+    for i in word:
+        result = int(i.split()[-1]) + 10
+        print(result)
+
+
+result_sum(answer)
