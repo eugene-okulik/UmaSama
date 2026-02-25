3591f93c9e53af884875e51edde9b1e4e49efd68
umasama <umidkerimov2@gmail.com>
2026-02-25 09:55:37 +0300

    Выполнение домашнего задания №10

--- homework/umid_kerimov/homework_10/task3.py
+++ homework/umid_kerimov/homework_10/task3.py
@@ -2,11 +2,11 @@ def smart_operation(calc_func):
     def wrapper(num1, num2):
         if num1 == num2:
             return calc_func(num1, num2, '+')
-        elif num1 > num2:
+        elif num1 < 0 or num2 < 0:
             return calc_func(num1, num2, '*')
-        elif num2 > num1:
-            return calc_func(num1, num2, '-')
         elif num1 > num2:
+            return calc_func(num1, num2, '-')
+        elif num2 > num1:
             return calc_func(num1, num2, '/')
 
     return wrapper
